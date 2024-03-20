8c285bbfccf525e3a45f51a48af98af74c5662bc
Paweł Pietrzak <pawel.pietrzak9617@gmail.com>
2024-03-20 14:22:33 +0100

    Added H2 station database

--- src/main/kotlin/com/pietrzak/mystation/persistence/StationRepository.kt
+++ src/main/kotlin/com/pietrzak/mystation/persistence/StationRepository.kt
@@ -1,6 +1,7 @@
 package com.pietrzak.mystation.persistence
 
 import org.springframework.stereotype.Repository
+import org.springframework.data.repository.CrudRepository
 
 @Repository
-interface StationRepository
+interface StationRepository: CrudRepository<Station, Long>
